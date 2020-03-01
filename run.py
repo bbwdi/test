9f662016e429c66c431d6388e08524911eab78cd
bbwdi <37570284+bbwdi@users.noreply.github.com>
2020-03-01 21:40:57 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,1 +1,99 @@
-https://github.com/bbwdi/test.githttps://github.com/bbwdi/test.githttps://github.com/bbwdi/test.git
+#coding:utf-8
+from HTMLTestRunner import HTMLTestRunner
+from setting import *
+from common.function import *
+import unittest
+import time,os
+from selenium import  webdriver
+
+#======新增加========
+import smtplib
+from email.utils import formataddr
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+from email.header import Header
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def create__browser_driver(b='gc'):
+    try:
+        if b == 'gc':
+            dv = webdriver.Chrome()
+        elif b == 'ff':
+            dv = webdriver.Firefox()
+        elif b == 'ie':
+            dv = webdriver.Ie()
+        else:
+            pass
+        return dv
+    except BaseException:
+        pass
+#=================================================新增加=======================================================
+
+#====================定义发送邮件====================
+def send_mail(file_new):
+    host = 'genlot.com'
+    port ='25'
+    from_user = 'xiaolin.zou@genlot.com'  # 发送这邮箱
+    passwd = '666888zxl'     # 密码--SMTP授权码
+    to_user = [u'568505593@qq.com',u'baoming.zheng@genlot.com',
+               u'lin.duan@genlot.com',u'henglin.wu@genlot.com',
+               u'ranfeng.gong@genlot.com',u'zhengxin.huang@genlot.com',
+               u'xiaoming.chen@genlot.com',u'yansong.li@genlot.com',
+               u'weipeng.gao@genlot.com',u'jie.liu@genlot.com',
+               u'2489039801@qq.com',u'657365253@qq.com',
+               u'yonghong.zhou@genlot.com',u'nanshan.chen@genlot.com'
+               ] # 接收邮箱
+    # to_user = u'568505593@qq.com'  # 接收邮箱
+
+    f = open(file_new,'rb')
+    mail_body = f.read()
+    f.close()
+    #msg = MIMEText(mail_body,'html','utf-8')
+    #创建一个带附件的实例
+    msg=MIMEMultipart()
+    msg['Subject'] = Header('自动化测试报告','utf-8')
+    msg['From']=formataddr(["小林",from_user])  # 发件人邮箱昵称、发件人邮箱账号
+    message = MIMEMultipart()
+    #构造附件
+    attach_file=MIMEText(open(file_new, 'rb').read(), 'base64', 'utf-8')
+    attach_file["Content-Type"] = 'application/octet-stream'
+    attach_file["Content-Disposition"] = 'attachment; filename="report.html"'#filename可以任意写，写什么名字，邮件中显示什么名字
+    msg.attach(attach_file)
+    msg.attach(MIMEText(mail_body,'html','utf-8'))#邮件正文内容
+    try:
+        #smtp = smtplib.SMTP_SSL(host,port)
+        smtp = smtplib.SMTP(host,port)
+        smtp.login(from_user,passwd)
+        smtp.sendmail(from_user,to_user,msg.as_string())
+        smtp.quit()
+        print(u'邮件发送成功！')
+    except  smtplib.SMTPException as e:
+        print(u'邮件发送失败！')
+        print(e)
+
+#=================查找最新的测试报告==================
+def new_report(testReport):
+    '''读取最新的报告'''
+    lists = os.listdir(testReport)
+    lists.sort(key = lambda fn:os.path.getmtime(testReport + fn))
+    file_new = os.path.join(testReport,lists[-1])
+    return file_new
+
+#=================================================新增加=======================================================
+
+
+if __name__ == '__main__':
+    login_verificcation()
+    now = time.strftime("%Y_%m_%d_%H_%M_%S")
+    filename =html_report + u"test_result_" + now + u".html"
+    fp = open(filename, "wb")
+    runner = HTMLTestRunner(fp, title=u"测试报告", description = u"Chrome测试结果")
+    discover = unittest.defaultTestLoader.discover(case_path,pattern=u"*test.py",top_level_dir=case_path)
+    runner.run(discover)
+    #新增加
+    fp.close()
+    file_path = new_report(html_report)
+    send_mail(file_path)
---
+++ setting.py
@@ -0,0 +1,41 @@
+# coding:utf-8
+import os
+
+RECERVIE = ['henglin.wu@genlot.com',
+              'liangzhang.lin@genlot.com',
+              'mingli.li@genlot.com',
+              'ou.gong@genlot.com',
+              'dan.deng@genlot.com',
+              'zhong.zhang@genlot.com'
+              'xiong.wang@genlot.com'
+        ]
+#数据库连接
+connetion_info = "U_VLT_BMS/123456@10.6.0.202/ORCL"
+
+#站点信息
+station_id = '41010101'
+#登录用户
+username1= u'中心负责人2'
+username2= 'chenxiaoming'
+
+tt='D:\\zxl_project\\VLT\\genlot_vlt'
+
+download_path=u'C:/Users/Go/Downloads/'
+case_path=u'D:/zxl_project/VLT/genlot_vlt/cases/'
+html_report=u'D:/zxl_project/VLT/genlot_vlt/static/report/'
+role_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/role_number.csv'#角色编号
+game_path=u'D:/vlt_game_package/'
+upload_path=u'D:/zxl_project/VLT/genlot_vlt/static/upload/'
+plan_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/plan_number.csv'
+config_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/config_number.csv'
+name_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/game_name_number.csv'
+game_name_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/game_name.csv'
+card_code_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/card_code.csv'
+terminal_serial_file=tt + u'\\static\\data\\terminal_serail_number.csv'#终端机
+teller_serial_file=tt + u'\\static\\data\\teller_serial_number.csv'#柜员机
+tag_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/tag_number.csv'
+task_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/task_number.csv'
+rights_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/rights_number.csv'
+subject_number_file=u'D:/zxl_project/VLT/genlot_vlt/static/data/subject_number.csv'
+
+
